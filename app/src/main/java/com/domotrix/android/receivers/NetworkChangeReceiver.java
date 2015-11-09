1925c1c9e7cd4a521bf11c70b0009775e23ada06
cougaro <zingaro>
2015-11-09 16:25:58 +0100

    working on SDK

--- app/src/main/java/com/domotrix/android/receivers/NetworkChangeReceiver.java
+++ app/src/main/java/com/domotrix/android/receivers/NetworkChangeReceiver.java
@@ -2,7 +2,6 @@ package com.domotrix.android.receivers;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.RemoteException;
 import android.util.Log;
 
 public class NetworkChangeReceiver extends BasicRemoteReceiver {
@@ -10,19 +9,12 @@ public class NetworkChangeReceiver extends BasicRemoteReceiver {
 
   @Override
   public void onReceive(final Context context, final Intent intent) {
-    if (connectToRemoteService(context)) {
-      ServiceManager serviceManager = new ServiceManager(context);
-      if (serviceManager.isNetworkAvailable()) {
+    ServiceManager serviceManager = new ServiceManager(context);
+    if (serviceManager.isNetworkAvailable()) {
         String networkName = serviceManager.getNetworkName();
         Log.d(TAG,"YOU ARE CONNECTED ON :"+networkName);
         if (isConnected()) {
-          try {
-            mService.remoteLog(TAG,"YOU ARE CONNECTED ON :"+networkName);
-          } catch (RemoteException e) {
-          }
         }
-      }
     }
-    disconnectFromRemoteService(context);
   }
 }
